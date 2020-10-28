c567625ffef43fa02a2d745688214450252fb28c
tiago <tiago86g@gmail.com>
2020-10-28 17:04:01 +0100

    UsersList componen. Fixes small things

--- src/components/UserList/UsersList.js
+++ src/components/UserList/UsersList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import "./UserList.css"
+import "./UsersList.css"
 
 export default function UsersList({ users }) {
   const [data, setData] = useState([]);
@@ -19,7 +19,7 @@ export default function UsersList({ users }) {
   }, [users]);
 
   if (!users) {
-    return <p>No users</p>;
+    return <p className="UsersList_error-message">No users yet</p>;
   }
   if (!data) {
     return (
@@ -33,7 +33,7 @@ export default function UsersList({ users }) {
     return <p>Loading...</p>;
   } else {
     return (
-      <ul >
+      <ul className="UsersList">
         {data.map(user => {
           return (
             <li key={user.id}>
@@ -41,12 +41,12 @@ export default function UsersList({ users }) {
                 <img
                   src={user.avatar_url}
                   alt={user.login}
-                  className="UsersName_image"
+                  className="UsersList_image"
                 />
               </span>
               <Link
                 to={`/${user.login}`}
-                className="UsersName_link">
+                className="UsersList_link">
                 <span>{user.login}</span>
               </Link>
             </li>
