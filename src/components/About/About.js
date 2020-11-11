6758cc1e816dc62383d9e33ce2307f1b3f79869d
tiago <tiago86g@gmail.com>
2020-11-11 12:42:56 +0100

    add github icon to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import "./About.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function About() {
   const imageUrl = "https://avatars3.githubusercontent.com/u/54849930?s=400&u=2625d054497cc3d8fb4e323eb520ac226928b27e&v=4";
   const gitHubUrl = 'https://github.com/tiago86g/GutHub_user-search';
   return (
-    <div className="About">
+    <div className="about">
       <div className="block-center">
-        <img className="About_image" src={imageUrl} alt="Tiago" />
+        <img className="about_image" src={imageUrl} alt="Tiago" />
       </div>
-      <h1 className="About_main-name">About me</h1>
+      <h1 className="about_main-name">About me</h1>
       <div>
-       <p className="About_details"> Brazilian web developer and designer with a clear strategic approach to problem solving.
+       <p className="about_details"> Brazilian web developer and designer with a clear strategic approach to problem solving.
 With a background as a designer, I am highly skilled in structuring complex layers of information. I also have vast experience adapting data to different languages.</p>
 
-<p className="About_details">I'm a teamplayer by nature and very self-motivated. Moreover, I truly care about winning as a team and enjoy the collaboration process with other developers because it makes all of us better. </p> 
+<p className="about_details">I'm a teamplayer by nature and very self-motivated. Moreover, I truly care about winning as a team and enjoy the collaboration process with other developers because it makes all of us better. </p> 
       </div>
       
-      <div className="About_github-page block-center">
+      <div className="about_github-page block-center">
           <a href={gitHubUrl} alt={`link to this project GitHub page`}target={gitHubUrl}>
-            <button className="About_button">Open on GitHub</button>
+            <button className="about-github-button">
+              <FontAwesomeIcon icon={faGithub} size="4x" className="tiago-github-icon"/>
+            </button>
           </a>
         </div>
     </div>
