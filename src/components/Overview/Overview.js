b00923a19bbcd9c03e8980852c8707c3d3540c8d
tiago <tiago86g@gmail.com>
2020-11-11 11:18:34 +0100

    add fontAwesome and add GitHub icon to overview page

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import SearchInput from "../SearchInput/SearchInput";
 import UsersList from "../UserList/UsersList";
-import "./Overview.css"
+import "./Overview.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Overview() {
     const [users, setUsers] = useState("")
@@ -10,6 +12,7 @@ export default function Overview() {
         <>
             <div className="Overview_main-image">
                 <div className="github">
+                <FontAwesomeIcon icon={faGithub} size="6x" className="github-icon"/>
                 </div>
             </div>
             <SearchInput users={users} setUsers={setUsers}/>
