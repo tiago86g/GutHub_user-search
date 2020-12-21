c7926a9ec69ae6a708361b573b672fc367891cd3
tiago86g <tiago86g@gmail.com>
2020-12-21 20:21:14 +0100

    footer feature

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithubAlt,
+  faLinkedinIn,
+  faBehance,
+} from '@fortawesome/free-brands-svg-icons';
+import {} from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
-    return (
-        <div className="Footer">
-        
-        </div>
-    )
+  return (
+    <footer>
+      <div class="some">
+        <a href="https://www.github.com/tiago86g">
+          <FontAwesomeIcon icon={faGithubAlt} />
+        </a>
+        <a href="https://www.linkedin.com/in/tiago86g/">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href="https://www.behance.net/tiago86g">
+          <FontAwesomeIcon icon={faBehance} />
+        </a>
+      </div>
+    </footer>
+  );
 }
